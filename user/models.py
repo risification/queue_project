d22b18bed75cf99ed1fd7b31d47dd755ef3f46d0
risification <sultangaziev01@bk.ru>
2021-06-06 23:05:17 +0600

    add Client, Worker

--- user/models.py
+++ user/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=50)
+    phone = models.IntegerField(default=0)
+
+
+class Worker(models.Model):
+    full_name = models.CharField(max_length=50)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.IntegerField()
+    to_book = models.ForeignKey(Client, on_delete=models.CASCADE)
